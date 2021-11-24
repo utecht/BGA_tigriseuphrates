7a491492381638d11272a60146cba4b883eff9c8
Joseph Utecht <thenmal@gmail.com>
2021-11-24 15:44:16 -0600

    first pass at state machine

--- tigriseuphrates/states.inc.php
+++ tigriseuphrates/states.inc.php
@@ -49,6 +49,19 @@
 
 //    !! It is not a good idea to modify this file when a game is running !!
 
+if (!defined('STATE_END_GAME')) { // guard since this included multiple times
+   define("STATE_PLAYER_TURN", 2);
+   define("STATE_CHECK_REVOLT", 3);
+   define("STATE_INCREMENT_ACTION", 4);
+   define("STATE_TURN_CLEANUP", 5);
+   define("STATE_REVOLT_SUPPORT", 6);
+   define("STATE_CHECK_WAR", 7);
+   define("STATE_WAR_SUPPORT", 8);
+   define("STATE_SELECT_WAR_LEADER", 9);
+   define("STATE_BUILD_MONUMENT", 10);
+   define("STATE_END_GAME", 99);
+}
+
  
 $machinestates = array(
 
@@ -58,46 +71,91 @@ $machinestates = array(
         "description" => "",
         "type" => "manager",
         "action" => "stGameSetup",
-        "transitions" => array( "" => 2 )
+        "transitions" => array( "" => STATE_PLAYER_TURN )
     ),
     
     // Note: ID=2 => your first state
 
-    2 => array(
+    STATE_PLAYER_TURN => array(
     		"name" => "playerTurn",
-    		"description" => clienttranslate('${actplayer} must play a card or pass'),
-    		"descriptionmyturn" => clienttranslate('${you} must play a card or pass'),
+    		"description" => clienttranslate('${actplayer} must take an action'),
+    		"descriptionmyturn" => clienttranslate('${you} must take an action'),
     		"type" => "activeplayer",
-    		"possibleactions" => array( "playCard", "pass" ),
-    		"transitions" => array( "playCard" => 2, "pass" => 2 )
+    		"possibleactions" => array( "placeLeader", "placeTile", "placeCatastrophe", "discard" ),
+    		"transitions" => array( "placeLeader" => STATE_CHECK_REVOLT, "placeTile" => STATE_CHECK_WAR, "placeCatastrophe" => STATE_INCREMENT_ACTION, "discard" => STATE_INCREMENT_ACTION)
     ),
-    
-/*
-    Examples:
-    
-    2 => array(
-        "name" => "nextPlayer",
-        "description" => '',
-        "type" => "game",
-        "action" => "stNextPlayer",
-        "updateGameProgression" => true,   
-        "transitions" => array( "endGame" => 99, "nextPlayer" => 10 )
-    ),
-    
-    10 => array(
-        "name" => "playerTurn",
-        "description" => clienttranslate('${actplayer} must play a card or pass'),
-        "descriptionmyturn" => clienttranslate('${you} must play a card or pass'),
-        "type" => "activeplayer",
-        "possibleactions" => array( "playCard", "pass" ),
-        "transitions" => array( "playCard" => 2, "pass" => 2 )
-    ), 
 
-*/    
-   
+    STATE_CHECK_REVOLT => array(
+            "name" => "checkRevolt",
+            "description" => clienttranslate('Checking for a revolt'),
+            "type" => "game",
+            "updateGameProgression" => false,
+            "transitions" => array( "revoltFound" => STATE_REVOLT_SUPPORT, "safe" => STATE_INCREMENT_ACTION )
+    ),
+
+    STATE_REVOLT_SUPPORT => array(
+            "name" => "supportRevolt",
+            "description" => clienttranslate('${actplayer} may support revolt'),
+            "descriptionmyturn" => clienttranslate('${you} may support revolt'),
+            "type" => "activeplayer",
+            "possibleactions" => array( "placeSupport", "pass" ),
+            "transitions" => array( "placeSupport" => STATE_REVOLT_SUPPORT, "pass" => STATE_REVOLT_SUPPORT, "revoltConcluded" => STATE_INCREMENT_ACTION )
+    ),
+
+    STATE_CHECK_WAR => array(
+            "name" => "checkWar",
+            "description" => clienttranslate('Checking for a war'),
+            "type" => "game",
+            "updateGameProgression" => false,
+            "transitions" => array( "warFound" => STATE_SUPPORT_WAR, "multiWarFound" => STATE_SELECT_WAR_LEADER, "safeNoMonument" => STATE_INCREMENT_ACTION, "safeMonument" => STATE_BUILD_MONUMENT )
+    ),
+
+    STATE_WAR_SUPPORT => array(
+            "name" => "supportWar",
+            "description" => clienttranslate('${actplayer} may support war'),
+            "descriptionmyturn" => clienttranslate('${you} may support war'),
+            "type" => "activeplayer",
+            "possibleactions" => array( "placeSupport", "pass" ),
+            "transitions" => array( "placeSupport" => STATE_WAR_SUPPORT, "pass" => STATE_WAR_SUPPORT, "warConcluded" => STATE_CHECK_WAR )
+    ),
+
+    STATE_SELECT_WAR_LEADER => array(
+            "name" => "warLeader",
+            "description" => clienttranslate('${actplayer} must select war leader'),
+            "descriptionmyturn" => clienttranslate('${you} must select war leader'),
+            "type" => "activeplayer",
+            "possibleactions" => array( "selectWarLeader" ),
+            "transitions" => array( "selectWarLeader" => STATE_WAR_SUPPORT )
+    ),
+
+    STATE_BUILD_MONUMENT => array(
+            "name" => "buildMonument",
+            "description" => clienttranslate('${actplayer} may build monument'),
+            "descriptionmyturn" => clienttranslate('${you} may build monument'),
+            "type" => "activeplayer",
+            "possibleactions" => array( "buildMonument", "pass" ),
+            "transitions" => array( "buildMonument" => STATE_INCREMENT_ACTION, "pass" => STATE_INCREMENT_ACTION )
+    ),
+
+    STATE_INCREMENT_ACTION => array(
+            "name" => "incrementAction",
+            "description" => clienttranslate('Incrementing Action'),
+            "type" => "game",
+            "updateGameProgression" => true,
+            "transitions" => array( "endTurn" => STATE_TURN_CLEANUP, "secondAction" => STATE_PLAYER_TURN )
+    ),
+
+    STATE_TURN_CLEANUP => array(
+            "name" => "turnCleanup",
+            "description" => clienttranslate('Ending turn for ${actplayer}'),
+            "type" => "game",
+            "updateGameProgression" => true,
+            "transitions" => array( "nextPlayer" => STATE_PLAYER_TURN, "endGame" => STATE_END_GAME )
+    ),
+    
     // Final state.
     // Please do not modify (and do not overload action/args methods).
-    99 => array(
+    STATE_END_GAME => array(
         "name" => "gameEnd",
         "description" => clienttranslate("End of game"),
         "type" => "manager",
