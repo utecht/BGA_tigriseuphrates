ef055098fc1c0d81d96a4b4ca5f7f960d910bdb5
Joseph Utecht <thenmal@gmail.com>
2022-07-27 14:54:36 -0500

    added point names to log

--- tigriseuphrates/modules/php/Notifications/Score.php
+++ tigriseuphrates/modules/php/Notifications/Score.php
@@ -7,7 +7,7 @@ class Score extends \TAE\Core\Notifications {
 	public static function playerScore($color, $points, $player_id, $player_name, $source, $id, $animate) {
 		self::notifyAll(
 			"playerScore",
-			'${player_name} scored ${points} <div class="point ${color}_point"></div>',
+			'${player_name} scored ${points} <div class="point ${color}_point"><span class="log_point">${color}</span></div>',
 			array(
 				'player_id' => $player_id,
 				'player_name' => $player_name,
